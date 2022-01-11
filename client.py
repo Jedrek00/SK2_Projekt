2b203ac3f9b5cd7e778db5d6d728a55de651eaae
KowalewskiBartlomiej <bartlomiej.p.kowalewski@student.put.poznan.pl>
2022-01-11 19:48:52 +0100

    Update client

--- client.py
+++ client.py
@@ -1,8 +1,6 @@
 import tkinter as tk
 import socket
-#import threading
 import argparse
-from tkinter.constants import LEFT, RAISED
 
 #Wczytywanie adresu serwera i nr portu jako argumentów przy uruchamianiu 
 parser = argparse.ArgumentParser()
@@ -10,25 +8,32 @@ parser.add_argument("server_address")
 parser.add_argument("port")
 args = parser.parse_args()  
 
-#Stworzenie okna aplikacji
-root = tk.Tk()
-root.geometry('600x400')
-root.title("Publish/subscribe Client")
+def add_window(size = '300x200', title = "Client App"):
+    window = tk.Tk()
+    window.geometry(size)
+    window.title(title)
+    return window
 
-frame = tk.Frame(master=root)
-frame.pack(fill=None)
+def add_label(anchor, txt = ""):
+    label = tk.Label(master = anchor, text = txt)
+    label.pack()
+    return label
+
+def add_entry(anchor):
+    entry = tk.Entry(master = anchor)
+    entry.pack()
+    return entry
+
+def add_button(anchor, txt = ""):
+    button = tk.Button(master = anchor, text = txt)
+    button.pack(fill="both")
+    return button
 
 def buttons():
     for i in "Dodaj temat", "Wyślij wiadomość", "Zasubskrybuj temat", "Anuluj subskrybcję", "Wyjście":
-        b = tk.Button(master=frame, text=i)
+        b = tk.Button(master=root, text=i)
         b.pack(side="top", fill="both")
         yield b
-
-b1, b2, b3, b4, b5 = buttons()
-
-label = tk.Label(master = root)
-label.pack()
-
 
 #Funkcja łącząca z serwerem
 def connect(server_addr, port):
@@ -70,30 +75,42 @@ def add_topic(akcja, sockfd, entry, view):
         error_label.pack()
         exit_button = tk.Button(master = error_view, text="OK", command=error_view.destroy)
         exit_button.pack()
-    else: 
+    else:
+        print(error)
+        error_view = tk.Tk()
+        error_view.title("Komunikat")
+        error_label = tk.Label(master = error_view, text=error)
+        error_label.pack()
+        exit_button = tk.Button(master = error_view, text="OK", command=error_view.destroy)
+        exit_button.pack()
         view.destroy()
 
-#Funkcja odpowiadająca za wprowadzanie nowych danych
-def insert_new_topic():
-    inserting_view = tk.Tk()
-    inserting_view.title("Wprowadzanie tematu")
-    inserting_view.geometry('300x200')
-    lab = tk.Label(master = inserting_view, text="Wprowadź nazwę tematu: ")
-    lab.pack()
-    entry = tk.Entry(master = inserting_view)
-    entry.pack()
-    butt1 = tk.Button(master = inserting_view, text="Zatwierdź")
-    butt2 = tk.Button(master = inserting_view, text="Anuluj")
-    butt1.pack()
-    butt2.pack()
-    butt1.configure(command=lambda:add_topic("a", sockfd, entry, inserting_view))
-    butt2.configure(command=inserting_view.destroy)
+#Funkcja odpowiadająca za wprowadzanie tematu
+def insert_topic():
+    inserting_window = add_window()
+    label = add_label(inserting_window, "Wprowadź nazwę tematu: ")
+    entry = add_entry(inserting_window)
+    apply_button = add_button(inserting_window, "Zatwierdź")
+    cancel_button = add_button(inserting_window, "Anuluj")
+    apply_button.configure(command=lambda:add_topic("a", sockfd, entry, inserting_window))
+    cancel_button.configure(command=inserting_window.destroy)
 
 #Łączenie się z serwerem
 sockfd = connect(args.server_address, int(args.port))
 
+#Stworzenie okna aplikacji
+root = add_window('600x400', "Publish/subscribe Client")
+
+frame = tk.Frame(master=root)
+frame.pack(fill=None)
+
+b1, b2, b3, b4, b5 = buttons()
+
+label = tk.Label(master = root)
+label.pack()
+
 #Przypisanie akcji do przycisków
-b1.configure(command=insert_new_topic)
+b1.configure(command=insert_topic)
 b5.configure(command=lambda:close_app(sockfd))
 
 #Stworzenie okna
